63f76ec051d9672c5614135dd78c48c5b5e96cea
Farida Memon <fmemon100@yahoo.com>
2011-06-02 14:20:42 -0400

    Added help static page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,11 +4,7 @@ describe PagesController do
   render_views
   
   before(:each) do
-<<<<<<< HEAD
-    @base_title = "Ruby on Rails Tutorial Sample App | "
-=======
     @base_title = "Ruby on Rails Tutorial Sample App"
->>>>>>> static-pages
   end
   
   describe "GET 'home'" do
@@ -19,11 +15,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-<<<<<<< HEAD
-      response.should have_selector("title", :content => @base_title + "Home")
-=======
       response.should have_selector("title", :content => @base_title + " | Home")
->>>>>>> static-pages
     end
   end
 
@@ -35,11 +27,7 @@ describe PagesController do
 
      it "should have the right title" do
        get 'contact'
-<<<<<<< HEAD
-       response.should have_selector("title", :content => @base_title + "Contact")
-=======
        response.should have_selector("title", :content => @base_title + " | Contact")
->>>>>>> static-pages
      end
    end
 
@@ -52,11 +40,7 @@ describe PagesController do
 
      it "should have the right title" do
        get 'about'
-<<<<<<< HEAD
-       response.should have_selector("title", :content => @base_title + "About")
-=======
        response.should have_selector("title", :content => @base_title + " | About")
->>>>>>> static-pages
      end
    end
 
@@ -65,20 +49,10 @@ describe PagesController do
        get 'help'
        response.should be_success
      end
-<<<<<<< HEAD
-=======
 
       it "should have the right title" do
         get 'help'
         response.should have_selector("title", :content => @base_title + " | Help")
-      end
-    end
-#Testing if gut is working
->>>>>>> static-pages
-
-      it "should have the right title" do
-        get 'help'
-        response.should have_selector("title", :content => @base_title + "Help")
       end
     end
     
