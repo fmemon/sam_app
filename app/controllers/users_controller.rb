26794ed4f587eb5dece8bc7d2ced8063376a1176
Farida Memon <fmemon100@yahoo.com>
2011-06-05 00:21:03 -0400

    All done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate, :except => [:show, :new, :create]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
   
@@ -13,7 +13,18 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
-
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
   def new
     @user= User.new
     @title = "Sign up"
@@ -51,6 +62,8 @@ class UsersController < ApplicationController
     flash[:success] = "User destroyed."
     redirect_to users_path
   end
+
+  
   private
 
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,15 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships,  :foreign_key => "follower_id",
+                            :dependent => :destroy
+                            
+  has_many :reverse_relationships,  :foreign_key => "followed_id",
+                                    :class_name => "Relationship",
+                                    :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower                                  
+                            
+  has_many :following, :through => :relationships, :source => :followed                          
     
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -28,6 +37,20 @@ class User < ActiveRecord::Base
                         :length       => {:within => 6..40}
   
   before_save :encrypt_password
+  
+  def self.authenticate_with_salt(id, stored_salt)
+  end
+  
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
   
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
@@ -46,7 +69,7 @@ class User < ActiveRecord::Base
   
   def feed
     # This is preliminary. See Chapter 12 for the full implementation
-    Micropost.where("user_id = ?",id)
+    Micropost.from_users_followed_by(self)
   end
     
   private
